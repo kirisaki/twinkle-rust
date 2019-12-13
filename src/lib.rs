552de576fb9354c15b913cb16c75d4c07e2f0c3d
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-12-13 21:42:54 +0900

    feat: listen

--- src/lib.rs
+++ src/lib.rs
@@ -1,95 +1,110 @@
-use std::net::{UdpSocket};
-use std::convert::TryFrom;
+use tokio::net::UdpSocket;
+use uuid::Uuid;
+use std::collections::HashMap;
+use std::sync::mpsc::{Sender, Receiver, channel};
+use futures::future;
+use tokio::sync::Mutex;
+use std::sync::{Arc};
+
 
 // limitation of UDP
 const BUF_SIZE: usize = 64 * 1024;
+const UUID_LEN: usize = 16;
+
+type Bytes = Vec<u8>;
+type Table = HashMap<Uuid, Sender<Bytes>>;
 
 pub struct Client {
-    socket: UdpSocket,
+    sock: UdpSocket,
+    tabl: Table,
+}
+
+#[derive(Debug, PartialEq)]
+enum Request {
+    Ping,
+    Get(Bytes),
+    Set(Bytes, Bytes),
+    Unset(Bytes),
 }
 
 pub async fn open(tx: String) -> Result<Client, std::io::Error> {
-    let s = UdpSocket::bind("127.0.0.1:3000")?;
-    s.connect(tx);
-    Ok (Client {
-        socket: s
-    })
+    let sock = UdpSocket::bind("127.0.0.1:0").await?;
+    sock.connect(tx).await?;
+    let tabl = HashMap::new();
+    let c = Client{sock, tabl};
+    Ok(c)
 }
+
 
 impl Client {
-    pub async fn ping(&self) -> Result<(), std::io::Error> {
-        let mut req: Vec<u8> = vec![0x00];
-
-        let _ = self.socket.send(req.as_mut_slice())?;
-
-        let mut buf = [0; BUF_SIZE];
-        let amt = self.socket.recv(&mut buf)?;
-        return Err(std::io::Error::new(std::io::ErrorKind::Other, buf[1].to_string()));
-
-        match parse_body(&buf, amt) {
-            Some((_, _, _)) => Ok(()),
-            None => Err(std::io::Error::new(std::io::ErrorKind::Other, "failed parsing")),
-        }
+    pub async fn ping(&mut self) -> Result<(), std::io::Error> {
+        let rx = Request::Ping.issue().dispatch(self).await?;
+        println!("{:?}", rx.recv());
+        Ok(())
     }
 
-    pub async fn get(&self, mut key: Vec<u8>) -> Result<Vec<u8>, std::io::Error> {
-        let keylen: i16 = TryFrom::try_from(key.len()).unwrap();
-        let h: u8 = TryFrom::try_from(keylen/256).unwrap();
-        let l: u8 = TryFrom::try_from(keylen).unwrap();
-        let mut req: Vec<u8> = vec![];
-        let mut header: Vec<u8> = vec![0x01, h, l];
-        req.append(&mut header);
-        req.append(&mut key);
-        let _ = self.socket.send(req.as_mut_slice())?;
+    pub async fn listen(&mut self) -> Result<(), std::io::Error> {
+        let Client{sock, tabl} = self;
+        let mut buf = vec![0; BUF_SIZE];
+        loop {
+            let amt = sock.recv(&mut buf).await?;
+            let e = std::io::Error::new(std::io::ErrorKind::Other, "failed parsing");
+            if amt < UUID_LEN + 1 {
+                return Err(e);
+            } else {
+                if buf[0] == 0x02 {
+                    return Err(e);
+                } else {
+                    let uuid = Uuid::from_slice(&buf[1..UUID_LEN]).unwrap();
+                    let val = buf[UUID_LEN + 1..amt].to_vec();
+                    match tabl.get(&uuid) {
+                        Some(c) => {c.send(val); return Ok(())},
+                        None => return Err(e),
+                    }
+                }
+            }
 
-        let mut buf = [0; BUF_SIZE];
-        let amt = self.socket.recv(&mut buf)?;
-
-        match parse_body(&buf, amt) {
-            Some((_, _, value)) => Ok(value.to_vec()),
-            None => Err(std::io::Error::new(std::io::ErrorKind::Other, "failed parsing")),
         }
     }
 }
 
-fn parse_body<'a>(buf: &'a [u8; BUF_SIZE], amt: usize) -> Option<(u8, &'a [u8], &'a [u8])> {
-    if amt >= 3 {
-        let cmd = buf[0];
-        let high: usize = From::from(buf[1]);
-        let low: usize = From::from(buf[2]);
-        let keylen = high * 256 + low;
-        let key = if keylen == 0 {
-            &[]
-        } else {
-            &buf[3..3+keylen]
+
+impl Request {
+    fn issue(self) -> (Packet) {
+        let uuid = Uuid::new_v4();
+        let mut cont = match self {
+            Request::Ping =>
+                vec![0x01],
+            _ => unimplemented!(),
         };
-        let value = if 3 + keylen == amt {
-            &[]
-        } else {
-            &buf[3+keylen..amt]
-        };
-        return Some((cmd, key, value));
-    } else if amt == 1 {
-        return Some((buf[0], &[], &[]));
-    } else {
-        return None;
+        cont.append(&mut uuid.as_bytes().to_vec());
+        Packet{cont, uuid}
     }
 }
 
+struct Packet {
+    cont: Bytes,
+    uuid: Uuid,
+}
+
+impl Packet {
+    async fn dispatch(self,  c: &mut Client) -> Result<Receiver<Bytes>, std::io::Error> {
+        let Packet{cont, uuid} = self;
+        let (tx, rx) = channel();
+        c.tabl.insert(uuid, tx);
+        c.sock.send(&cont).await?;
+        Ok(rx)
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use crate::*;
 
     #[tokio::test]
-    async fn it_works() {
-        let c = open("127.0.0.1:3000".to_string()).await.unwrap();
-        assert!(match c.ping().await {
-            Ok(_) => true,
-            Err(e) => {
-                println!("error: {:?}", e);
-                false
-            },
-        })
+    async fn it_works() -> Result<(), std::io::Error> {
+        let mut client = open("127.0.0.1:3000".to_string()).await?;
+        future::try_join(client.listen(), client.ping());
+        Ok(())
     }
 }

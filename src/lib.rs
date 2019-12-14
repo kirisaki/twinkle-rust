86a5263c847612d3836b45ff179819224e0bca24
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-12-14 12:34:45 +0900

    feat: send set and unset

--- src/lib.rs
+++ src/lib.rs
@@ -96,21 +96,42 @@ impl Request {
                 c
             },
             Request::Get(bytes) => {
-                let keylen = bytes.len();
-                if keylen > From::from(u16::max_value()){
-                  return  Err(std::io::Error::new(std::io::ErrorKind::Other, "keylen too long"))
-                } else {
-                    let mut keybytes = keylen.to_be_bytes();
-                    let mut c = vec![0x02];
-                    c.append(&mut uuid.as_bytes().to_vec());
-                    c.append(&mut keybytes[6..8].to_vec());
-                    c.append(&mut bytes.to_vec());
-                    c
-                }
+                let ks = usize_to_bytes(bytes.len())?;
+                let mut c = vec![0x02];
+                c.append(&mut uuid.as_bytes().to_vec());
+                c.append(&mut ks.to_vec());
+                c.append(&mut bytes.to_vec());
+                c
+            },
+            Request::Set(key, val) => {
+                let ks = usize_to_bytes(key.len())?;
+                let mut c = vec![0x03];
+                c.append(&mut uuid.as_bytes().to_vec());
+                c.append(&mut ks.to_vec());
+                c.append(&mut key.to_vec());
+                c.append(&mut val.to_vec());
+                c
+            },
+            Request::Unset(key) => {
+                let ks = usize_to_bytes(key.len())?;
+                let mut c = vec![0x04];
+                c.append(&mut uuid.as_bytes().to_vec());
+                c.append(&mut ks.to_vec());
+                c.append(&mut key.to_vec());
+                c
             },
             _ => unimplemented!(),
         };
         Ok(Packet{cont, uuid})
+    }
+}
+
+fn usize_to_bytes(u: usize) -> Result<[u8; 2], std::io::Error> {
+    if u > From::from(u16::max_value()){
+        Err(std::io::Error::new(std::io::ErrorKind::Other, "keylen too long"))
+    } else {
+        let ks = u.to_be_bytes();
+        Ok([ks[6], ks[7]])
     }
 }
 

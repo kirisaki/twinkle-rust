7ed90afbb941dfe4e28b33d10aa1e714b87df6c2
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-12-21 18:13:28 +0900

    feat: resend

--- src/client.rs
+++ src/client.rs
@@ -5,14 +5,12 @@ use std::io::{Error, ErrorKind};
 use tokio::net::{UdpSocket, ToSocketAddrs};
 use tokio::sync::Mutex;
 use tokio::sync::mpsc::{Sender, channel};
-use tokio::net::udp::SendHalf;
-use tokio::time::{timeout, delay_for, Duration};
+use tokio::time::{delay_for, Duration};
 
 use crate::dispatcher::*;
 use crate::listener::*;
 use crate::request::*;
 use crate::types::*;
-use crate::packet::*;
 
 #[derive(Clone)]
 pub struct Client {
@@ -22,7 +20,7 @@ pub struct Client {
 
 impl Client {
     pub async fn open<A: ToSocketAddrs>(addr: A) -> Result<(Client, Dispatcher, Listener), std::io::Error> {
-        let sock = UdpSocket::bind("127.0.0.1:0").await?;
+        let sock = UdpSocket::bind("0.0.0.0:0").await?;
         sock.connect(addr).await?;
         let (rxs, txs) = sock.split();
         let (txc, rxc) = channel(1024*1024*1024);
@@ -35,9 +33,19 @@ impl Client {
     }
 
     pub async fn ping(&mut self) -> Result<(), std::io::Error> {
-        let mut rx = Request::Ping.issue()?.dispatch(self).await?;
-        let _ = timeout(TIMEOUT, rx.recv()).await?;
-        Ok(())
+        for _ in 0..10 {
+            let packet = Request::Ping.issue()?;
+            let mut rx = packet.clone().dispatch(self).await?;
+            for i in 0..5u32 {
+                delay_for(Duration::from_millis(2u64.pow(i))).await;
+                match rx.try_recv() {
+                    Ok(_) => return Ok(()),
+                    Err(_) => {},
+                };
+
+            };
+        }
+        Err(Error::new(ErrorKind::Other, "request timeout"))
     }
     pub async fn get(&mut self, key: Bytes) -> Result<Bytes, std::io::Error> {
         for _ in 0..10 {
@@ -68,12 +76,20 @@ impl Client {
         }
         Err(Error::new(ErrorKind::Other, "request timeout"))
     }
-    pub async fn unset(&mut self, key: Bytes) -> Result<Bytes, std::io::Error> {
-        let mut rx = Request::Unset(key).issue()?.dispatch(self).await?;
-        match rx.recv().await {
-            Some(v) => v,
-            None => Err(Error::new(ErrorKind::Other, "key notfound")),
+    pub async fn unset(&mut self, key: Bytes) -> Result<(), std::io::Error> {
+        for _ in 0..10 {
+            let packet = Request::Unset(key.clone()).issue()?;
+            let mut rx = packet.clone().dispatch(self).await?;
+            for i in 0..5u32 {
+                delay_for(Duration::from_millis(2u64.pow(i))).await;
+                match rx.try_recv() {
+                    Ok(_) => return Ok(()),
+                    Err(_) => {},
+                };
+
+            };
         }
+        Err(Error::new(ErrorKind::Other, "request timeout"))
     }
 }
 
